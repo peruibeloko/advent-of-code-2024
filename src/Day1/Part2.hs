b2582a5dad91ddfcf14dbd624086783f96f94bda
peruibeloko <cmendonca32@gmail.com>
2024-12-14 21:33:33 -0300

    finish D1P2

--- src/Day1/Part2.hs
+++ src/Day1/Part2.hs
@@ -1,5 +1,29 @@
-module Day1.Part2 (solution) where
+module Day1.Part2 where
+
+import Data.List (sort)
+import qualified Data.Map.Strict as Map
+import Utils (count, frequencies, toInt)
 
 solution :: String -> String
 solution input =
-  input
+  show $ getSimilarityScore 0 $ getFrequencies $ buildLists ([], []) $ map lineToTuple $ lines input
+
+lineToTuple :: String -> (Maybe Int, Maybe Int)
+lineToTuple line =
+  case (words line) of
+    [a, b] -> (toInt a, toInt b)
+
+buildLists :: ([Int], [Int]) -> [(Maybe Int, Maybe Int)] -> ([Int], [Int])
+buildLists (l, r) [] = (sort l, sort r)
+buildLists (lOut, rOut) ((Just l, Just r) : xs) = buildLists ([l] ++ lOut, [r] ++ rOut) xs
+
+getFrequencies :: ([Int], [Int]) -> ([Int], Map.Map Int Int)
+getFrequencies (ls, rs) = (ls, frequencies rs)
+
+getSimilarityScore :: Int -> ([Int], Map.Map Int Int) -> Int
+getSimilarityScore result ([], _) = result
+getSimilarityScore score (l : ls, freqs) = getSimilarityScore newScore (ls, freqs)
+  where
+    newScore = case Map.lookup l freqs of
+      Just v -> score + l * v
+      Nothing -> score
--- src/Utils.hs
+++ src/Utils.hs
@@ -1,9 +1,18 @@
-module Utils (toInt, imSure) where
+module Utils where
 
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
 import Text.Read (readMaybe)
 
-toInt :: String -> Maybe Integer
-toInt x = readMaybe x :: Maybe Integer
+toInt :: String -> Maybe Int
+toInt x = readMaybe x :: Maybe Int
 
 imSure :: Maybe a -> a
 imSure (Just a) = a
+
+count :: (Eq a) => a -> [a] -> Int
+count x xs = length . filter (== x) $ xs
+
+frequencies :: (Ord a) => [a] -> Map.Map a Int
+frequencies xs =
+  foldl (\acc el -> Map.insert el (count el xs) acc) Map.empty (Set.elems $ Set.fromList xs)
